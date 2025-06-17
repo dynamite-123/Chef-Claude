419174a077d65d8b10c11b9a39d4e4f3c7936224
dynamite-123 <anirudhkashyap182005@gmail.com>
2025-06-17 10:08:58 +0530

    added header and body component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import chefClaudeLoco from '../assets/chef-claude-icon.png';
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLoco}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input aria-label="Add ingredient" type="text" placeholder="eg: Tomatoes" />
+                <button type="submit">Add ingredient</button>
+            </form>
+        </main>
+    )
+}
